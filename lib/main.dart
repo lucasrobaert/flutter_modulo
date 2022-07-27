5e15b04bd0f3cc116cb8aa5469bf522e85d13a55
Lucas Robaert <lucas@robaert.com.br>
2022-07-26 23:57:03 -0300

    chore: alteracao teste 2

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hello with module',
+              'Isso vai funcionar aqui',
             ),
             Text(
               '$_counter',
